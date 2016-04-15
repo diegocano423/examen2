9dec346fd19a4fa15c7493a9f9d8e10754f23ef6
David Guillen <maiden8-8@hotmail.com>
2016-04-14 21:51:13 -0600

    Esqueleto Lista

--- back-end/app/src/services/ListService.php
+++ back-end/app/src/services/ListService.php
@@ -2,19 +2,21 @@
 
 namespace App\Services;
 
-public function get () {
-    $ressultData = [];
-    $query = "SELECT * FROM videogames";
-    $params = [];
-        
-    $result = $this->storage->query($query, $params);
-        
-    if (count($result['data']) > 0) {
-        return $result['data'];
-    } else {
-        $result["message"] = "We don't have games at this moment.";
-        $result["error"] = true;
+class ListaService {
+    public function get () {
+        $data = [];
+        $query = "SELECT * FROM videogames";
+        $params = [];
+            
+        $result = $this->storage->query($query, $params);
+            
+        if (count($result['data']) > 0) {
+            return $result['data'];
+        } else {
+            $result["message"] = "We don't have games at this moment.";
+            $result["error"] = true;
+        }
+            
+        return $result;
     }
-        
-    return $result;
 }
